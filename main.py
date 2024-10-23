f97aecaef342698286dc62ddac3e4e6d8ca2af1c
unknown <jassim.alkuwari.97@gmail.com>
2024-10-23 08:19:29 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+import gradio as gr
+from fastai.vision.all import *
+
+# Load the trained model (make sure this path is correct or load from external URL if hosted)
+learn = load_learner('food_classifier.pkl')
+
+# Define the prediction function
+def predict_food(image):
+    image = image.resize((224, 224))  # Resize the image to match the input size of the model
+    pred, pred_idx, probs = learn.predict(image)
+    return {str(learn.dls.vocab[i]): float(probs[i]) for i in range(len(probs))}
+
+# Set up the Gradio interface
+image_input = gr.Image(type="pil")
+label_output = gr.Label(num_top_classes=3)
+
+interface = gr.Interface(fn=predict_food, 
+                         inputs=image_input, 
+                         outputs=label_output, 
+                         title="Food Classifier", 
+                         description="Upload a food image to classify it into one of 21 categories.")
+
+# Launch the app
+interface.launch(server_name="0.0.0.0", server_port=8080)
